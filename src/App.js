071e7b7b8998a704aed9e15572d8009b5c6d8414
Aakash Raghav <Aakash.Raghav@3pillarglobal.com>
2021-05-09 19:30:22 +0530

    Added version 2.0 changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello <code>src/App.js</code>
+          This is Version 2.0
         </p>
         <a
           className="App-link"
